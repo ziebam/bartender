1f3329605d12470ace67d92fc15df96deec7db9d
ziebam <ziebamichal@tutanota.com>
2021-11-10 17:33:14 +0100

    Set up command handling

---
+++ bartender.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const token = process.env.DISCORD_TOKEN;
+
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.login(token);
