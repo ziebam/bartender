68765cb1e78d19abe4212d26467e69a703ebdea5
ziebam <ziebamichal@tutanota.com>
2021-11-22 17:36:31 +0100

    Respond with the generated image (availability cmd)

--- commands/availability.js
+++ commands/availability.js
@@ -2,14 +2,6 @@ const fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const nodeHTMLToImage = require('node-html-to-image');
 
-const generateImage = (availability) => {
-	nodeHTMLToImage({
-		output: './test.png',
-		html: fs.readFileSync("./assets/availabilityTemplate.html", "utf8"),
-		content: { availability: availability }
-	})
-}
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('availability')
@@ -23,14 +15,15 @@ module.exports = {
 				.setName('next')
 				.setDescription('Shows the availability for the next week.')),
 	async execute(interaction) {
-		// TODO Rethink .json dynamic creation.
-
 		const parsedJSON = JSON.parse(fs.readFileSync('./mock_availability.json', 'utf8'));
 		// REVIEW Default to current week for invalid subcommands.
 		const availability = interaction.options.getSubcommand() === 'next' ? parsedJSON.next : parsedJSON.current;
 
-		generateImage(availability);
-
-		await interaction.reply("Done.");
+		await interaction.deferReply();
+		const buffer = await nodeHTMLToImage({
+			html: fs.readFileSync('./assets/availabilityTemplate.html', 'utf8'),
+			content: { availability: availability },
+		});
+		await interaction.editReply({ files: [buffer] });
 	},
 };
