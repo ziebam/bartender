70c0cb70df712c08974b3a699bbec649a0192fe8
ziebam <ziebamichal@tutanota.com>
2021-11-22 18:11:50 +0100

    Populate the template with week data

--- commands/availability.js
+++ commands/availability.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
+
 const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const moment = require('moment');
 const nodeHTMLToImage = require('node-html-to-image');
 
 module.exports = {
@@ -16,13 +19,22 @@ module.exports = {
 				.setDescription('Shows the availability for the next week.')),
 	async execute(interaction) {
 		const parsedJSON = JSON.parse(fs.readFileSync('./mock_availability.json', 'utf8'));
+		const nextWeek = interaction.options.getSubcommand() === 'next';
 		// REVIEW Default to current week for invalid subcommands.
-		const availability = interaction.options.getSubcommand() === 'next' ? parsedJSON.next : parsedJSON.current;
+		const availability = nextWeek ? parsedJSON.next : parsedJSON.current;
+
+		const time = moment();
+		if (nextWeek) time.add(1, 'w');
 
 		await interaction.deferReply();
+		const weekData = {
+			weekNumber: time.format('W'),
+			weekStart: time.clone().weekday(1).format('DD.MM.YY'),
+			weekEnd: time.clone().weekday(7).format('DD.MM.YY'),
+		};
 		const buffer = await nodeHTMLToImage({
 			html: fs.readFileSync('./assets/availabilityTemplate.html', 'utf8'),
-			content: { availability: availability },
+			content: { availability: availability, weekData: weekData },
 		});
 		await interaction.editReply({ files: [buffer] });
 	},
