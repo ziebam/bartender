a92cafbe851ec8a16f1a8a8615554408dd9ca3b8
ziebam <ziebamichal@tutanota.com>
2021-11-23 18:45:05 +0100

    Change tabs to 2 spaces
    The two main reasons are the odd tab size default when the code is
    rendered on GitHub and the nature of async code with lots of
    indentation.

--- bartender.js
+++ bartender.js
@@ -1,3 +1,5 @@
+// TODO Dynamic JSON creation.
+
 const dotenv = require('dotenv');
 dotenv.config();
 const token = process.env.DISCORD_TOKEN;
@@ -11,25 +13,25 @@ client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
 }
 
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+  if (!interaction.isCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
+  const command = client.commands.get(interaction.commandName);
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+  }
 });
 
 client.login(token);
--- commands/availability.js
+++ commands/availability.js
@@ -6,36 +6,36 @@ const moment = require('moment');
 const nodeHTMLToImage = require('node-html-to-image');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('availability')
-		.setDescription('Shows the availability.')
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('current')
-				.setDescription('Shows the availability for the current week.'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('next')
-				.setDescription('Shows the availability for the next week.')),
-	async execute(interaction) {
-		const parsedJSON = JSON.parse(fs.readFileSync('./mock_availability.json', 'utf8'));
-		const nextWeek = interaction.options.getSubcommand() === 'next';
-		// REVIEW Default to current week for invalid subcommands.
-		const availability = nextWeek ? parsedJSON.next : parsedJSON.current;
+  data: new SlashCommandBuilder()
+    .setName('availability')
+    .setDescription('Shows the availability.')
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('current')
+        .setDescription('Shows the availability for the current week.'))
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('next')
+        .setDescription('Shows the availability for the next week.')),
+  async execute(interaction) {
+    const parsedJSON = JSON.parse(fs.readFileSync('./mock_availability.json', 'utf8'));
+    const nextWeek = interaction.options.getSubcommand() === 'next';
+    // REVIEW Default to current week for invalid subcommands.
+    const availability = nextWeek ? parsedJSON.next : parsedJSON.current;
 
-		const time = moment();
-		if (nextWeek) time.add(1, 'w');
+    const time = moment();
+    if (nextWeek) time.add(1, 'w');
 
-		await interaction.deferReply();
-		const weekData = {
-			weekNumber: time.format('W'),
-			weekStart: time.clone().weekday(1).format('DD.MM.YY'),
-			weekEnd: time.clone().weekday(7).format('DD.MM.YY'),
-		};
-		const buffer = await nodeHTMLToImage({
-			html: fs.readFileSync('./assets/availabilityTemplate.html', 'utf8'),
-			content: { availability: availability, weekData: weekData },
-		});
-		await interaction.editReply({ files: [buffer] });
-	},
+    await interaction.deferReply();
+    const weekData = {
+      weekNumber: time.format('W'),
+      weekStart: time.clone().weekday(1).format('DD.MM.YY'),
+      weekEnd: time.clone().weekday(7).format('DD.MM.YY'),
+    };
+    const buffer = await nodeHTMLToImage({
+      html: fs.readFileSync('./assets/availabilityTemplate.html', 'utf8'),
+      content: { availability: availability, weekData: weekData },
+    });
+    await interaction.editReply({ files: [buffer] });
+  },
 };
